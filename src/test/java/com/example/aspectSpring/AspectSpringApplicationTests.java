e5d8c3071db31f63b8d0acb907c6dbbc0d159810
bookgvi <bookgvi@gmail.com>
2023-01-16 09:46:45 +0300

    git init

---
+++ src/test/java/com/example/aspectSpring/AspectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aspectSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
